22814d46557c1fda1caed8d84ee3c3b8fff9da04
hyfly233 <1274694743@qq.com>
2022-07-24 08:50:11 +0800

    commit

--- demo/src/main/java/org/example/demo/HolidayRequest.java
+++ demo/src/main/java/org/example/demo/HolidayRequest.java
@@ -1,15 +1,14 @@
 package org.example.demo;
 
-import org.flowable.engine.ProcessEngine;
-import org.flowable.engine.ProcessEngineConfiguration;
-import org.flowable.engine.RepositoryService;
-import org.flowable.engine.RuntimeService;
+import org.flowable.engine.*;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -59,6 +58,29 @@ public class HolidayRequest {
         variables.put("description", description);
 
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("holidayRequest", variables);
+
+        // ---------------------------------
+
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("managers").list();
+        System.out.println("You have " + tasks.size() + " tasks:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ") " + tasks.get(i).getName());
+        }
+
+        System.out.println("Which task would you like to complete?");
+        int taskIndex = Integer.valueOf(scanner.nextLine());
+        Task task = tasks.get(taskIndex - 1);
+        Map<String, Object> processVariables = taskService.getVariables(task.getId());
+        System.out.println(processVariables.get("employee") + " wants " + processVariables.get("nrOfHolidays") + " of holidays. Do you approve this?");
+
+        boolean approved = scanner.nextLine().equalsIgnoreCase("y");
+        variables = new HashMap<String, Object>();
+        variables.put("approved", approved);
+        taskService.complete(task.getId(), variables);
+
+        // ---------------------------------
+
 
 
         processEngine.close();
