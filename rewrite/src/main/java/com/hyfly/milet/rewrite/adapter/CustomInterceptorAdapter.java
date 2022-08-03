4c675f5f60174f51e47f1cef463f2523cfda696c
hyfly233 <1274694743@qq.com>
2022-08-03 09:58:38 +0800

    commit

--- rewrite/src/main/java/com/hyfly/milet/rewrite/adapter/CustomInterceptorAdapter.java
+++ rewrite/src/main/java/com/hyfly/milet/rewrite/adapter/CustomInterceptorAdapter.java
@@ -3,15 +3,14 @@ package com.hyfly.milet.rewrite.adapter;
 import com.hyfly.milet.rewrite.filter.FlowableHandlerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CustomInterceptorAdapter extends WebMvcConfigurerAdapter {
+public class CustomInterceptorAdapter implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new FlowableHandlerInterceptor())
                 .addPathPatterns("/**");
-        super.addInterceptors(registry);
     }
 }
